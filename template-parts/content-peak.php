2c7ef4066166da9de05b99fcbd9736f38ad85068
Coletta Fuller <fullerc21@mail.wlu.edu>
2019-11-27 15:01:16 -0500

    Ugly index page but displays thumbnails and links to actual single posts

---
+++ template-parts/content-peak.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template part for displaying results in search pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ShenAleph
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>  style = "width:300px">
+	<header class="entry-header">
+		<a href = <?php the_permalink(); ?>><div class="card" style = "width:300px">
+			<?php the_post_thumbnail(); ?>
+			<div class="card-body">
+				<h6 class="card-subtitle mb-2 text-muted">CONVERSATIONS</h6> <!-- this will get the category eventually-->
+				<h5 class="card-title"><?php the_title(); ?></h5>
+				<p class="card-text"><?php the_excerpt(); ?></p>
+			</div>
+    </div>
+  </a>
+	</header><!-- .entry-header -->
+	<footer class="entry-footer">
+		<?php shenAleph_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
