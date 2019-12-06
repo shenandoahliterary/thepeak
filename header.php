935e6bbdec878f195b1ccef34aa35e92d0f8a384
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2019-12-06 13:48:40 -0500

    Updated nav links

--- header.php
+++ header.php
@@ -27,11 +27,11 @@
 
 			<section class="topbanner container-fluid" id="typelogo-container">
 				<div class="row">
-								<a href="https://shenandoahliterary.org/682/" class="img-fluid-link" rel="home" itemprop="url"><img width="1600" height="228" src="https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600.jpg" class="img-fluid" alt="Shenandoah Volume 68, Number 1" itemprop="logo" srcset="https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600.jpg 1600w, https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600-300x43.jpg 300w, https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600-768x109.jpg 768w, https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600-1024x146.jpg 1024w" sizes="(max-width: 1600px) 100vw, 1600px" /></a>
+								<a href="https://shenandoahliterary.org/" class="img-fluid-link" rel="home" itemprop="url"><img width="1600" height="228" src="https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600.jpg" class="img-fluid" alt="Shenandoah Volume 68, Number 1" itemprop="logo" srcset="https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600.jpg 1600w, https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600-300x43.jpg 300w, https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600-768x109.jpg 768w, https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600-1024x146.jpg 1024w" sizes="(max-width: 1600px) 100vw, 1600px" /></a>
 				</div>
 				<div class="row">
 		<div class="col-md-10 offset-md-1">
-			<p id="typelogo"><a href="https://shenandoahLiterary.org/682/">Shenandoah</a></p>
+			<p id="typelogo"><a href="https://shenandoahLiterary.org/">Shenandoah</a></p>
 		</div>
 		</div>
 		<div class="row">
@@ -43,9 +43,9 @@
 			<div class="col-md-5">
 				<div class="shen-nav d-flex flex-column flex-sm-row" >
 					<a class="nav-item nav-link" href="https://shenandoahliterary.org/about/">About</a>
-					<a class="nav-item nav-link" href="https://shenandoahliterary.org/682/issues/">Issues</a>
-					<a class="nav-item nav-link" href="https://shenandoahliterary.org/681/submit/">Submit</a>
-					<a class="nav-item nav-link" href="https://shenandoahliterary.org/682/category/feature">The Peak</a>
+					<a class="nav-item nav-link" href="https://shenandoahliterary.org/issues/">Issues</a>
+					<a class="nav-item nav-link" href="https://shenandoahliterary.org/submit/">Submit</a>
+					<a class="nav-item nav-link" href="https://shenandoahliterary.org/thepeak">The Peak</a>
 				</div>
 				</div>
 		</div>
