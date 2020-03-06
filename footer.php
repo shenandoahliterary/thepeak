9944354434bc73d4637baf26ad36e2ee0a4cd1f4
Coletta MacBook Air <fullerc21@mail.wlu.edu>
2020-03-05 21:35:06 -0500

    mostly okay footer

--- footer.php
+++ footer.php
@@ -13,18 +13,21 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer container">
-		<hr>
-		<div class="site-info row">
-			<div class="col-md-12">
-				<p class="text-right"><a href="https://www.facebook.com/ShenandoahLiterary"><i class="fab fa-facebook-square"></i></a> <a href="https://www.instagram.com/shenandoah_literary"><i class="fab fa-instagram"></i></a> <a href="https://twitter.com/ShenandoahWLU"><i class="fab fa-twitter"></i></a>   </p>
-<a href = "https://shenandoahliterary.org/"><img class="img-fluid float-left shen-logo-footer" src="https://shenandoahliterary.org/681/files/2018/12/shen-w175.png"></a><a href = "https://www.wlu.edu/"><img class="img-fluid wlu-logo" src="https://shenandoahliterary.org/681/files/2018/12/wlu-w300.png"></a>
-<a href = "https://www.clmp.org/"><img class="img-fluid float-right" src="https://shenandoahliterary.org/681/files/2018/12/clmp.png" href = "https://www.clmp.org/"></a>
+	<footer id="colophon">
+			<div class="container-fluid row" style="background-color: #66aca9; padding-top:-2rem;">
+				<div class="col-lg-3 text-center"><p class = "footimages" id="sides"><a href="https://www.facebook.com/ShenandoahLiterary"><i class="fab fa-facebook-square"></i></a>
+					<a href="https://www.instagram.com/shenandoah_literary"><i class="fab fa-instagram"></i></a>
+					<a href="https://twitter.com/ShenandoahWLU"><i class="fab fa-twitter"></i></a></p>
+				</div>
+				<div class="col-lg-6 text-center">
+					<p class = "footimages" id="middle"><a href = "https://www.wlu.edu/"><img style = "height:80px;" src="https://shenandoahliterary.org/681/files/2018/12/wlu-w300.png"></a></p>
+				</div>
+				<div class="col-lg-3 text-center">
+					<p class = "footimages" id="sides"><a href = "https://www.clmp.org/"><img src="https://shenandoahliterary.org/681/files/2018/12/clmp.png" href = "https://www.clmp.org/"></a></p>
+				</div>
+					<a href="https://shenandoahliterary.org/682/" class="img-fluid-link" rel="home" itemprop="url"><img width="1600" height="228" style = "margin-top: -5rem" src="https://shenandoahliterary.org/wp-content/uploads/2020/01/transparent.png" class="img-fluid" alt="Shenandoah Volume 68, Number 1" itemprop="logo" srcset="https://shenandoahliterary.org/wp-content/uploads/2020/01/transparent.png 1600w, https://shenandoahliterary.org/wp-content/uploads/2020/01/transparent.png 300w, https://shenandoahliterary.org/wp-content/uploads/2020/01/transparent.png 768w, https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600-1024x146.jpg 1024w" sizes="(max-width: 1600px) 100vw, 1600px" /></a>
 			</div>
-<div class="row">
-	<div class="col-md-12">
 	<p class="copyright">&copy; 2018-2019 Shenandoah</p>
-</div>
 
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
