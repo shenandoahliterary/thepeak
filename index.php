6b936360b9ddb6b46e35f5d4e8f4ba48597915d3
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2023-04-27 12:34:04 -0400

    Update index.php
    Removed justify-content-md-center from row definition. Not needed.

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main container">
-			<section class="row justify-content-md-center" data-masonry='{"percentPosition": true }'>
+			<section class="row" data-masonry='{"percentPosition": true }'>
 			
 		<?php
 		if ( have_posts() ) :
