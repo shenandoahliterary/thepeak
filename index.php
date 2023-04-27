73d10e17d1a0d35d6276f8b0d7f595aa4f3e4c0a
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2023-04-27 12:31:20 -0400

    Update index.php
    Changed Bootstrap col definition to md-12

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ get_header();
 								 if ($count == "1") {
 									//change to col definition
 									//move col defi
-									?> <div class="col-md-8 justify-content-center"><?php
+									?> <div class="col-md-12 justify-content-center"><?php
  								}
 								 ?> 
 								<!--  orig location of col definition -->
